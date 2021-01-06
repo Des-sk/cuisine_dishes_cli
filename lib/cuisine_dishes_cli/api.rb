09e43cd2ebc428d9f98f184ac22197705b5ae84a
Des-sk <marquezdecardona@gmail.com>
2021-01-06 00:18:06 -0400

    api_cli

---
+++ lib/cuisine_dishes_cli/api.rb
@@ -0,0 +1,48 @@
+class API
+
+    
+
+
+    # def self.get_data
+    #     response = RestClient.get("https://api.spoonacular.com/recipes/complexSearch?")
+    #     api_key = "ebd49d55da6c4a0f9e6a5098a077595b"
+    #     dishes_array = JSON.parse(response)
+    #     binding.pry
+    # end
+
+    def self.get_data
+    domain = "https://api.spoonacular.com"
+    base_path = "/recipes/complexSearch"
+    api_key = "ebd49d55da6c4a0f9e6a5098a077595b"
+    results_size = 10
+    response = RestClient.get("#{domain}#{base_path}?apiKey=#{api_key}&cuisines_name=#{cuisines_name}&number=#{results_size}", {
+            headers: {"Content-Type" => "application/json"},
+        })
+        dishes_array = JSON.parse(response)["results"]
+        dishes_array .each do |dish|
+            Dish.new(dish)
+
+        
+            binding.pry
+        end
+    end
+
+        
+    def self.cuisines_name
+        cuisines_name = ["african", "american", "british", "cajun", "caribbean", "chinese", "eastern european", "european", "french", "german", "greek", "indian", "irish", "italian", "japanese", "jewish", "korean", "latin american", "mediterranean", "mexican", "middle eastern", "nordic", "southern", "spanish", "thai", "vietnamese"]
+        cuisines_name .each do |cuisine|
+            Cuisine.new(cuisine)
+        end
+    end
+
+    # def get_dishes
+    #     if response["status"] == 'failure'
+    #         puts response["message"]
+    #     else
+    #         dishes = response["results"]     
+    #     end       
+    # end
+end
+
+ 
+
---
+++ lib/cuisine_dishes_cli/cli.rb
@@ -0,0 +1,68 @@
+class CLI
+
+    def start
+       puts "Hi, and welcome to the Cuisine Inspo cli!"
+       puts "Input Cuisines to see a list of cuisine styles from around de world or exit to leave."
+       API.get_data
+       #binding.pry
+       menu
+     #binding.pry
+    end
+ 
+    def user_input
+       gets.strip
+    end
+ 
+    def cuisines_list
+       Cuisine.all.each.with_index(1)do |cuisine, i|
+       puts "#{i}. #{cuisine.name}"
+       end
+       select_cuisine
+       
+    end
+ 
+    def goodbye
+       puts "Get Coocking!!!"
+    end
+ 
+    def error
+       puts "Try Again"
+    end
+ 
+ 
+    def select_cuisine
+       puts "Select a cuisine style to see some inspirational dishes."
+       menu
+    end  
+       
+    
+    def dishes_list
+       Dish.all
+       
+    end 
+   
+ 
+    def menu
+       selection = user_input 
+ 
+       if selection == "#{cuisine.name}"
+          dishes_list
+ 
+       elsif selection == "Cuisines"
+          cuisines_list
+          menu
+ 
+       elsif selection == "exit"
+          goodbye
+ 
+       else
+          error
+          menu
+       end
+      
+    end
+ end
+ 
+ 
+ 
+ 
