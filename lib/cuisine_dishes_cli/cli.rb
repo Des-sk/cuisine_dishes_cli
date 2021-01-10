0b42306371c52c0c161cdc26ebd7c1730e38f367
Des-sk <marquezdecardona@gmail.com>
2021-01-10 16:27:29 -0400

    final

--- lib/cuisine_dishes_cli/cli.rb
+++ lib/cuisine_dishes_cli/cli.rb
@@ -1,9 +1,15 @@
 class CLI
 
     def start
+      puts " "
+      puts " "
       puts "Hi, and welcome to the Cuisine Inspo cli!"
+      puts " "
+      puts " "
       puts "Input Cuisines to see a list of cuisine styles from around de world or exit to leave."
+      puts " "
       Cuisine.cuisines_name
+      puts " "
       menu
     end
      
@@ -13,24 +19,32 @@ class CLI
     end
  
     def cuisines_list
+      puts " "
        Cuisine.all.each.with_index(1)do |cuisine, i|
-       puts "#{i}. #{cuisine.name}"
+       puts "#{i}.#{cuisine.name}  "
        end
        select_cuisine 
     end
  
     def goodbye
-       puts "Get Cooking!!!"
+      puts " "
+      puts " "
+      puts "Get Cooking!!!"
+      puts " "
+      puts " "
     end
  
     def error
-       puts "Cuisine not supported. See the list of supported Cuisines again:"
+       puts "Cuisine not supported! See the list of supported Cuisines again:"
         cuisines_list
     end
  
  
     def select_cuisine
+        puts " "
+        puts " "
         puts "Input a Cuisine name to se a list of inspirational dishes" 
+        puts " "
          get_dishes
     end
 
@@ -38,6 +52,7 @@ class CLI
     def get_dishes
       Dish.all.clear
       selection = user_input
+      puts " "
        API.get_data(selection)
        dishes_list
     end
@@ -48,9 +63,13 @@ class CLI
         Dish.all.each do |dish|
          puts "#{dish.title}"
         end
-         puts "Would you like to see dishes from another Cuisine? Enter Cuisines if you do or exit if you're done"
+         puts " "
+         puts " "
+         puts "Would you like to see dishes from another Cuisine? Enter Cuisines if you do or exit if you're done" 
+         puts " "
          menu
       else 
+         puts " "
          error
          #get_dishes
       end 
